bf425f94957de40a16b94730f363b285c40df1c5
River <hom0906@naver.com>
2024-01-05 15:33:28 +0900

    UndeadSurvivor_InputManager

--- Unity Theory/UndeadSurvivor/Player.cs
+++ Unity Theory/UndeadSurvivor/Player.cs
@@ -59,3 +59,20 @@ namespace Unity_Theory.UndeadSurvivor
 
             rigid.MovePosition(rigid.position + nextVec);
         }
+        /*
+        Input Manager 사용
+        [패키지 다운로드] -> [새로운 액션 생성]
+        스크립트에 적용하기 위해서는 반드시 UnityEngine.InputSystem 네임스페이스를 추가해 주어야 한다.
+
+        value.Get<Vector2>()는 InputSystem에서 입력 값을 가져오는 메서드
+
+        OnMove(InputValue value) 메서드는 InputSystem의 이벤트로서, 
+        플레이어의 이동 입력을 감지하는 역할을 합니다. 
+        이벤트가 발생할 때, InputSystem은 플레이어가 움직이는 방향을 Vector2 형태로 value 매개변수에 담아서 호출
+        value.Get<Vector2>()는 이렇게 전달된 Vector2 값을 실제로 가져와서 사용
+
+        예를 들어, 플레이어가 화살표 키를 누르면 value.Get<Vector2>()는 (1, 0) 또는 (-1, 0)과 같은 
+        Vector2 값을 반환하여 플레이어의 움직임 방향을 알려준다.
+        이 방향 값을 이용하여 플레이어를 이동시키거나, 
+        특정 방향으로 공격을 발사하는 등의 동작을 수행한다.
+         */
